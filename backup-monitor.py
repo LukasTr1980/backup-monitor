e96f23ad78595b46cd616a5c00ccb441b70b7b4a
LukasTr1980 <lukas.troebinger@gmail.com>
2023-03-12 09:27:39 +0100

    Added file size of the most recent backup file in tar.gz format

--- backup-monitor.py
+++ backup-monitor.py
@@ -7,6 +7,12 @@ import sys
 
 BACKUP_DIR = "/home/pi/influx/backup"
 
+last_backup_file = max((os.path.join(BACKUP_DIR, f) for f in os.listdir(BACKUP_DIR) if f.endswith('.tar.gz')), default=None)
+if last_backup_file:
+    last_backup_file_size = os.path.getsize(last_backup_file)
+else:
+    last_backup_file_size = 0
+
 last_modified = max((os.path.getmtime(os.path.join(BACKUP_DIR, f)) for f in os.listdir(BACKUP_DIR)), default=0)
 last_modified_date = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(last_modified))
 last_modified_unix = int(time.mktime(time.localtime(last_modified)))
@@ -14,8 +20,8 @@ elapsed = time.time() - last_modified
 interval = 4 * 24 * 60 * 60
 
 if elapsed < interval:
-    result = {"status": 0, "last_modified_date": last_modified_unix}
+    result = {"status": 0, "last_modified_date": last_modified_unix, "last_backup_file_size": last_backup_file_size}
 else:
-    result = {"status": 1, "last_modified_date": last_modified_unix}
+    result = {"status": 1, "last_modified_date": last_modified_unix, "last_backup_file_size": last_backup_file_size}
 
 print(json.dumps(result))
